eb89072dc1a4f28514b259be410177f56fa25134
unknown <jamesmamo@gmail.com>
2018-01-29 22:44:30 +0100

    initial commit

---
+++ app/mock-heroes.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.HEROES = [
+    { "id": 10, "name": "Hadoop" },
+    { "id": 11, "name": "Rails" },
+    { "id": 12, "name": "Chronos" },
+    { "id": 13, "name": "Storm" },
+    { "id": 14, "name": "Spark" },
+];
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export var HEROES: Hero[] = [
+    { "id": 10, "name": "Hadoop" },
+    { "id": 11, "name": "Rails" },
+    { "id": 12, "name": "Chronos" },
+    { "id": 13, "name": "Storm" },
+    { "id": 14, "name": "Spark" },
+]
